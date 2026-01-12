5023d7850626af885b42b8f5ff8e395d6e623118
Jean Luc Bazimya <bazimyajean123@gmail.com>
2026-01-11 19:40:12 -0600

    Updated requirement

--- server/main.py
+++ server/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Campus Ride Backend", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-        "http://127.0.0.1:5173",],
+        "http://127.0.0.1:5173","https://curious-rolypoly-15ca78.netlify.app/", "https://curious-rolypoly-15ca78.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
